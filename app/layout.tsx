4154bf28b162399b19affcccd36a0c5f19668083
Jaya <jaya.lohana24@gmail.com>
2024-06-16 00:01:25 -0400

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   title: "Zenith",
   description: "An AI powered image generator",
 };
-//dai
+
 export default function RootLayout({
   children,
 }: Readonly<{
