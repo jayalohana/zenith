6792ef18a76b5dcb3de985538596413e3441c788
Jaya Lohana <jaya.lohana24@gmail.com>
2025-08-12 00:00:00 -0400

    commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,6 +6,17 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
 export default Layout;
 
+
+//daily
+//daily
+//daily
+//daily
+//daily
+//daily
+//daily
+//daily
+//daily
+//daily
 //daily
 //daily
 //daily
